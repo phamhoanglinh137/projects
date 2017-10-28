c3360f4c696ac28eacd13bdeb68800e2d3abc2f5
phamhoanglinh137 <phamhoanglinh137@gmail.com>
2017-10-28 20:16:40 +0800

    update sql

--- sp_group/database/schema-mysql.sql
+++ sp_group/database/schema-mysql.sql
@@ -1,15 +1,15 @@
-DROP SCHEMA IF EXISTS rds_mysql_db;
+DROP SCHEMA IF EXISTS sp_db;
 
-CREATE SCHEMA IF NOT EXISTS rds_mysql_db DEFAULT CHARACTER SET utf8;
+CREATE SCHEMA IF NOT EXISTS sp_db DEFAULT CHARACTER SET utf8;
 USE rds_mysql_db;
 
-CREATE TABLE IF NOT EXISTS rds_mysql_db.sp_user (
+CREATE TABLE IF NOT EXISTS sp_db.sp_user (
   user_id varchar(36) not null,
   email_address VARCHAR(100) NOT NULL,
   PRIMARY KEY (user_id)
 );
 
-CREATE TABLE IF NOT EXISTS rds_mysql_db.sp_user_relationship (
+CREATE TABLE IF NOT EXISTS sp_db.sp_user_relationship (
   user_id varchar(36) not null,
   friend_id varchar(36) not null,
   type varchar(10) not null,
