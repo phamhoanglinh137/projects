a463ae4dbce67a960800c5249fc1cd7d3bd12f27
phamhoanglinh137 <phamhoanglinh137@gmail.com>
2017-12-22 12:50:50 +0800

    java 8 - stream updates

--- java8/readme.md
+++ java8/readme.md
@@ -1,13 +1,15 @@
 Main features in java 8
-(more details : http://www.oracle.com/technetwork/java/javase/8-whats-new-2157071.html)
 
-youtube : https://www.youtube.com/playlist?list=PLsyeobzWxl7qbvNnJKjYbkTLn2w3eRy1Q
+http://www.oracle.com/technetwork/java/javase/8-whats-new-2157071.html
+https://www.youtube.com/playlist?list=PLsyeobzWxl7qbvNnJKjYbkTLn2w3eRy1Q
+https://zeroturnaround.com/rebellabs/java-8-streams-cheat-sheet/
+http://winterbe.com/posts/2014/07/31/java8-stream-tutorial-examples/
 
 1. forEach() method in Iterable interface
 2. default and static methods in Interfaces
 3. Functional Interfaces and Lambda Expressions
    http://www.baeldung.com/java-8-functional-interfaces
- - Functional Interface is an interface with only one abstract method 
+ - Functional Interface is an interface with only one abstract method
    regardless of whether or not a FunctionalInterface annotation is present on the interface declaration.
    for example: Runnable, Comparable
   Main interfaces :
@@ -17,6 +19,7 @@ youtube : https://www.youtube.com/playlist?list=PLsyeobzWxl7qbvNnJKjYbkTLn2w3eRy
   - Consumer: accepts one more than one arguments and return void
   - Operator: argument and return are same type
 4. Java Stream API for Bulk Data Operations on Collections
+   ![Stream summary](stream.png)
 5. Java Time API
 6. Collection API improvements
 7. Concurrency API improvements
--- java8/src/com/feature/stream/StreamFeature.java
+++ java8/src/com/feature/stream/StreamFeature.java
@@ -1,39 +1,132 @@
 package com.feature.stream;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.IntStream;
 
 /**
  * 
- * source --> stream --> immediate functions --> terminal functions
- * Main interfaces :
- * - Optional : wrap to avoid null pointer
- * - Predicate : one or more than one arguments and return boolean
- * - Function<T,R> : accept one argument and produces a result R
- * - Consumer: accepts one more than one arguments and return void
- * - Operator: argument and return are same type
+ * source --> stream / parallelStream --> intermediate operation --> terminal operation 
  * 
- * Main immediate functions: 
- * - filter
- * - map
- * - sort
+ * Main interfaces : 
+ *  - Optional : wrap to avoid null pointer - Predicate : one or more than one arguments and return boolean 
+ *  - Function<T,R> : accept one argument and produces a result R 
+ *  - Consumer: accepts one more than one arguments and return void 
+ *  - Operator: argument and return are same type
  * 
+ * Main intermediate functions: 
+ * - filter 
+ * - map 
+ * - sort 
+ * - peek 
+ * - distinct 
+ * - flatmap
+ * 
+ * Main terminal
+ * - reduce
+ * - collect
  * 
  * @author linhpham
  *
  */
 public class StreamFeature {
-
+	public static void main(String args[]) {
+		
+		intermediateFunc();
+		
+		flatmap();
+		
+		reduce();
+		
+		collect();
+		
+	}
+	
+	/**
+	 * sample for intermediate functions.
+	 * print staff name if age > 5 
+	 */
+	public static void intermediateFunc() {
+		System.out.println("-- Intermediate Func - start");
+		
+		staffList(10).stream()
+		.filter(staff -> staff.getAge() > 5)
+		.map(staff -> staff.getName())
+		.sorted((str1, str2) -> str1.compareTo(str2))
+		.forEach(name -> System.out.println("name :" + name));
+		
+		System.out.println("-- Intermediate Func-- end");
+	}
+	
+	/**
+	 * get all distinct staff's bankcard.
+	 */
+	public static void flatmap() {
+		System.out.println("-- Flat Map -- start");
+		
+		staffList(5).stream()
+				.map(staff -> staff.bankCards)
+				.peek(staff -> IntStream.range(1, 5).forEach(j -> staff.add("DBS Credit " + j)))
+				.flatMap(str -> str.stream()).distinct()
+				.forEach(System.out::println);
+		
+		System.out.println("-- Flat Map -- end");
+	}
+	
+	/** 
+	 * join all distinct credit card name together 
+	 * 
+	 */
+	public static void reduce() {
+		System.out.println("-- Reduce - start");
+		
+		staffList(5).stream()
+		.map(staff -> staff.bankCards)
+		.peek(staff -> IntStream.range(1, 5).forEach(j -> staff.add("DBS Credit " + j)))
+		.flatMap(str -> str.stream()).distinct().reduce((base, card) -> base + card).ifPresent(s -> System.out.println(s));
+		
+		System.out.println("-- Reduce -- end");
+	}
+	
+	public static void collect() {
+		System.out.println("-- Collect - start");
+		
+		System.out.println("-- Collect - end");
+	}
+	
+	public static void parallel() {
+		System.out.println("-- Parallel - start");
+		staffList(10).parallelStream().skip(1).forEach(str -> System.out.println(str.getName()));
+		System.out.println("-- Parallel - end");
+	}
+	
+	public static List<Staff> staffList(int size) {
+		List<Staff> staffs = new ArrayList<Staff>();
+		IntStream.range(1, size).forEach(i -> staffs.add(new Staff(i, "Staff " + i, "Dev " + i)));
+		return staffs;
+	}
+	
 	public static class Staff {
 
 		private int age;
 		private String name;
 		private String jobTittle;
+		
+		private Set<String> bankCards = new HashSet<String>();
 
 		public Staff(int age, String name, String jobTittle) {
 			this.age = age;
 			this.name = name;
 			this.jobTittle = jobTittle;
+		}
+		
+		public void add(String bankCard) {
+			bankCards.add(bankCard);
+		}
+		public Set<String> bankCards() {
+			return bankCards;
 		}
 
 		public int getAge() {
@@ -82,25 +175,7 @@ public class StreamFeature {
 			return true;
 		}
 	}
-
-	public static void main(String args[]) {
-		staffList().stream()
-		.filter(staff -> staff.getAge() > 25)
-		.map(staff -> staff.getName())
-		.sorted((str1, str2) -> str1.compareTo(str2))
-		.forEach(action -> System.out.println("name :" + action));
-		
-		staffList().parallelStream().skip(1).forEach(str -> System.out.println(str.getName()));
-	}
+	
 	
 
-	public static List<Staff> staffList() {
-		List<Staff> staffs = new ArrayList<Staff>();
-		staffs.add(new Staff(25, "Staff 2", "Dev 2"));
-		staffs.add(new Staff(23, "Staff 1", "Dev 1"));
-		staffs.add(new Staff(27, "Staff 4", "Dev 4"));
-		staffs.add(new Staff(27, "Staff 3", "Dev 3"));
-		
-		return staffs;
-	}
 }
