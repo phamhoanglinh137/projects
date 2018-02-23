3d75cd01034713c3154e04746f917f52b9fb72fb
phamhoanglinh137 <phamhoanglinh137@gmail.com>
2018-02-23 15:48:29 +0800

    add registry + logstash

---
+++ microservice/registry/src/main/java/com/registry/RegistryApp.java
@@ -0,0 +1,18 @@
+package com.registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 
+ * @author linhpham
+ *
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class RegistryApp {
+	public static void main(String[] args) {
+		SpringApplication.run(RegistryApp.class, args);
+	}
+}
